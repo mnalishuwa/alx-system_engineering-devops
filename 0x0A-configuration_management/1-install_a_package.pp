b285d1c1e24756c4e8997e0a944bebffe0cdc9e3
Mukwa Nalishuwa <mnalishuwa@gmail.com>
2023-12-20 20:53:47 +0000

    Update puppet install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 # install flask from pip3
 
+package {'python3-pip':
+  ensure => 'installed',
+}
+
 $packages = ['flask', '2.1.0', 'werkzeug', '2.1.1']
 
 package { $packages:
   ensure => 'installed',
+  require => Package['python3-pip'],
 }
